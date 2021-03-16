2b79a6b9a7ba85ca2cdd4e02ac920ded9801ce9d
AlexNazaroff <ale.krotov@gmail.com>
2021-03-16 07:20:46 +0000

    for i in range

--- 2.py
+++ 2.py
@@ -11,3 +11,16 @@ pab()
 
 podscet_monet_i_ih_ves(100,2.5,1)
 podscet_monet_i_ih_ves(200,5,2)
+
+
+
+a1 = 100
+b1 = 200
+print('Площадь прямоугольника %s на %s равна %s' % (a1,b1,ploshad_axb(a1,b1)))
+
+for i in range (0,3):
+  print('Insert a:')
+  a = int(input())
+  print('Insert b:')
+  b = int(input())
+  print('Площадь прямоугольника %s на %s равна %s' % (a,b,ploshad_axb(a,b)))
